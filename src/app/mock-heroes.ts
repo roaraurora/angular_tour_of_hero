dcbc01335ee4393447cc75f54e2a48f9c1105729
roaraurora <dc83080779@gmail.com>
2018-05-31 00:02:40 +0800

    first day of  Tour of Hero

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr.Nice' },
+    { id: 12, name: 'Mr.Nice' },
+    { id: 13, name: 'Mr.Nice' },
+    { id: 14, name: 'Mr.Nice' },
+    { id: 15, name: 'Mr.Nice' },
+    { id: 16, name: 'Mr.Nice' },
+    { id: 17, name: 'Mr.Nice' },
+    { id: 18, name: 'Mr.Nice' },
+    { id: 19, name: 'Mr.Nice' },
+    { id: 20, name: 'Mr.Nice' },
+];
